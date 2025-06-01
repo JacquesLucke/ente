55e779e860b8b92a7b60a439924244afbb62c3ae
Jacques Lucke <mail@jlucke.com>
2025-06-01 19:51:42 +0200

    remove unrelated changes

--- mobile/lib/generated/intl/messages_tr.dart
+++ mobile/lib/generated/intl/messages_tr.dart
@@ -1389,6 +1389,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "onEnte": MessageLookupByLibrary.simpleMessage(
             "<branding>ente</branding> üzerinde"),
         "onTheRoad": MessageLookupByLibrary.simpleMessage("Yeniden yollarda"),
+        "onThisDay": MessageLookupByLibrary.simpleMessage("On this day"),
         "onlyFamilyAdminCanChangeCode": m54,
         "onlyThem": MessageLookupByLibrary.simpleMessage("Sadece onlar"),
         "oops": MessageLookupByLibrary.simpleMessage("Hay aksi"),
